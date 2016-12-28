4f06da731893fa3f5a65e807ed8baa9ae6157450
José Romero <jose.romero@vector-it.com.ar>
2016-12-28 12:24:26 -0300

    Cambios generales

--- js/vectorit.js
+++ js/vectorit.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
+    $(".btnTab").click(function() {
+        if (!$(this).hasClass("btnActivo")) {
+            $(".btnActivo").removeClass("btnActivo");
+            $(this).addClass("btnActivo");
+
+            var objeto = $(this).data("objeto");
+
+            if (objeto == 'Casa') {
+                $(".datosAuto").fadeOut(function() {
+                    $(".datosCasa").fadeIn();
+                });
+
+                $(".infoAuto").fadeOut(function() {
+                    $(".infoCasa").fadeIn();
+                });
+            }
+            else {
+                $(".datosCasa").fadeOut(function() {
+                    $(".datosAuto").fadeIn();
+                });
+
+                $(".infoCasa").fadeOut(function() {
+                    $(".infoAuto").fadeIn();
+                });
+            }
+        }
+    });
+
     $("#txtCUIT").change(function() {
         var cuit = $("#txtCUIT").val().trim();
         var url = "https://soa.afip.gob.ar/sr-padron/v2/persona/" + cuit;
@@ -14,7 +42,7 @@ $(document).ready(function () {
         });
     });
 
-    $("#frmPrestamo").submit(function() {
+    $("form").submit(function() {
 		if (($("#resultadoCuit").html().indexOf("Error") > -1) || ($("#resultadoCuit").html() == '')) {
 			alert("Antes de continuar debe ingresar un CUIT válido!");
 			$("#txtCUIT").focus();
@@ -25,18 +53,36 @@ $(document).ready(function () {
 
         setTimeout(function() {
             var mensaje = "";
-            mensaje+= "Registro de solicitud de crédito<br><br>";
+            var divOk = "";
+
+            if (this.id == "frmDatosCasa") {
+                divOk = "Casa";
+                mensaje+= "Registro de solicitud de crédito para vivienda<br><br>";
+            }
+            else {
+                divOk = "Auto";
+                mensaje+= "Registro de solicitud de crédito para auto<br><br>";
+            }
             mensaje+= "Apellido y Nombre: " + $('#resultadoCuit').html().trim() + "<br>";
             mensaje+= "CUIT: " + $("#txtCUIT").val().trim() + "<br>";
-            mensaje+= "Ingreso aproximado: " + $("#cmbIngreso").val().trim() + "<br>";
-            mensaje+= "Tipo de ingreso: " + $('input[name = "radIngreso"]:checked').val() + "<br>";
-            mensaje+= "Monto deseado: " + $("#txtMonto").val().trim() + "<br>";
             mensaje+= "Email: " + $("#txtEmail").val().trim() + "<br>";
             mensaje+= "Provincia: " + $("#cmbProvincia").val() + "<br>";
             mensaje+= "Ciudad: " + $("#txtCiudad").val().trim() + "<br>";
             mensaje+= "Telefono: " + $("#txtTelefono").val().trim() + "<br>";
             mensaje+= "Horario de contacto: de " + $("#horaDesde").val() + " a " + $("#horaHasta").val() + "<br>";
-            //mensaje+= "Domicilio: " + $("#txtDomicilio").val().trim() + "<br>";
+
+            if (this.id == "frmCasa") {
+                mensaje+= "Ingreso aproximado: " + $("#cmbIngreso").val().trim() + "<br>";
+                mensaje+= "Tipo de ingreso: " + $('input[name = "radIngreso"]:checked').val() + "<br>";
+                mensaje+= "Tipologia: " + $("#txtMonto").val().trim() + "<br>";
+            }
+            else {
+                mensaje+= "Marca de auto: " + $("#cmbMarca").val().trim() + "<br>";
+                mensaje+= "Modelo de auto: " + $('#cmbModelo').val().trim() + "<br>";
+                mensaje+= "Dispone de usado: " + $("#txtMonto").val().trim() + "<br>";
+            }
+            mensaje+= "Valor de cuota que puede pagar: " + $("#txtCuota").val().trim() + "<br>";
+
             mensaje+= "Consulta: " + $("#txtConsulta").val().trim() + "<br>";
 
             var frmData = new FormData();
@@ -58,10 +104,10 @@ $(document).ready(function () {
                     $('#prestamoON').hide();
 
                     if (xmlhttp.responseText.indexOf('Error') == -1) {
-                        $('#frmPrestamo').hide();
+                        $('#frmDatos' + divOk).hide();
 
-                        $("#prestamoOK").fadeIn(function() {
-                            var targetOffset = $("#prestamoOK").offset().top-200;
+                        $("#prestamoOK" + divOk).fadeIn(function() {
+                            var targetOffset = $("#prestamoOK" + divOk).offset().top-200;
                             $('html,body').animate({scrollTop: targetOffset}, 500);
                         });
                     }
