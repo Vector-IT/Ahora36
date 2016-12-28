de1b7c364adf5e58e1e6de9b507d4bbf38e096ae
José Romero <jose.romero@vector-it.com.ar>
2016-12-28 14:51:01 -0300

    Cambios varios de funcionamiento

--- js/vectorit.js
+++ js/vectorit.js
@@ -175,24 +175,24 @@ $(document).ready(function () {
                     $(".datosCasa").fadeIn();
                 });
 
-                $(".infoAuto").fadeOut(function() {
+                /*$(".infoAuto").fadeOut(function() {
                     $(".infoCasa").fadeIn();
-                });
+                });*/
             }
             else {
                 $(".datosCasa").fadeOut(function() {
                     $(".datosAuto").fadeIn();
                 });
 
-                $(".infoCasa").fadeOut(function() {
+                /*$(".infoCasa").fadeOut(function() {
                     $(".infoAuto").fadeIn();
-                });
+                });*/
             }
         }
     });
 
-    $("#txtCUIT").change(function() {
-        var cuit = $("#txtCUIT").val().trim();
+    $(".txtCUIT").change(function() {
+        var cuit = $(".txtCUIT").val().trim();
         var url = "https://soa.afip.gob.ar/sr-padron/v2/persona/" + cuit;
 
         $.get(url, function(data) {
@@ -200,16 +200,24 @@ $(document).ready(function () {
                 $(".resultadoCuit").html(data.data.nombre);
             }
             else {
-                $("#resultadoCuit").html("Error al procesar su CUIL/CUIT, por favor verifique el dato ingresado.");
-                $("#txtCUIT").focus();
+                $(".resultadoCuit").html("Error al procesar su CUIL/CUIT, por favor verifique el dato ingresado.");
             }
         });
     });
 
     $("form").submit(function() {
-		if (($("#resultadoCuit").html().indexOf("Error") > -1) || ($("#resultadoCuit").html() == '')) {
+        var divOk = "";
+
+        if (this.id == "frmDatosCasa") {
+            divOk = "Casa";
+        }
+        else {
+            divOk = "Auto";
+        }
+
+		if (($("#resultadoCuit" + divOk).html().indexOf("Error") > -1) || ($("#resultadoCuit" + divOk).html() == '')) {
 			alert("Antes de continuar debe ingresar un CUIT válido!");
-			$("#txtCUIT").focus();
+			$("#txtCUIT" + divOk).focus();
 			return false;
 		}
 
@@ -217,41 +225,38 @@ $(document).ready(function () {
 
         setTimeout(function() {
             var mensaje = "";
-            var divOk = "";
 
-            if (this.id == "frmDatosCasa") {
-                divOk = "Casa";
+            if (divOk == "Casa") {
                 mensaje+= "Registro de solicitud de crédito para vivienda<br><br>";
             }
             else {
-                divOk = "Auto";
                 mensaje+= "Registro de solicitud de crédito para auto<br><br>";
             }
-            mensaje+= "Apellido y Nombre: " + $('#resultadoCuit').html().trim() + "<br>";
-            mensaje+= "CUIT: " + $("#txtCUIT").val().trim() + "<br>";
-            mensaje+= "Email: " + $("#txtEmail").val().trim() + "<br>";
-            mensaje+= "Provincia: " + $("#cmbProvincia").val() + "<br>";
-            mensaje+= "Ciudad: " + $("#txtCiudad").val().trim() + "<br>";
-            mensaje+= "Telefono: " + $("#txtTelefono").val().trim() + "<br>";
-            mensaje+= "Horario de contacto: de " + $("#horaDesde").val() + " a " + $("#horaHasta").val() + "<br>";
+            mensaje+= "Apellido y Nombre: " + $('#resultadoCuit' + divOk).html().trim() + "<br>";
+            mensaje+= "CUIT: " + $("#txtCUIT" + divOk).val().trim() + "<br>";
+            mensaje+= "Email: " + $("#txtEmail" + divOk).val().trim() + "<br>";
+            mensaje+= "Provincia: " + $("#cmbProvincia" + divOk).val() + "<br>";
+            mensaje+= "Ciudad: " + $("#txtCiudad" + divOk).val().trim() + "<br>";
+            mensaje+= "Telefono: " + $("#txtTelefono" + divOk).val().trim() + "<br>";
+            mensaje+= "Horario de contacto: de " + $("#horaDesde" + divOk).val() + " a " + $("#horaHasta" + divOk).val() + "<br>";
 
-            if (this.id == "frmCasa") {
-                mensaje+= "Ingreso aproximado: " + $("#cmbIngreso").val().trim() + "<br>";
-                mensaje+= "Tipo de ingreso: " + $('input[name = "radIngreso"]:checked').val() + "<br>";
-                mensaje+= "Tipologia: " + $("#txtMonto").val().trim() + "<br>";
+            if (divOk == "Casa") {
+                mensaje+= "Ingreso aproximado: " + $("#cmbIngreso" + divOk).val().trim() + "<br>";
+                mensaje+= "Tipo de ingreso: " + $('input[name = "radIngreso' + divOk + '"]:checked').val() + "<br>";
+                mensaje+= "Tipologia: " + $("#txtMonto" + divOk).val().trim() + "<br>";
             }
             else {
-                mensaje+= "Marca de auto: " + $("#cmbMarca").val().trim() + "<br>";
-                mensaje+= "Modelo de auto: " + $('#cmbModelo').val().trim() + "<br>";
-                mensaje+= "Dispone de usado: " + $("#txtMonto").val().trim() + "<br>";
+                mensaje+= "Marca de auto: " + $("#cmbMarca" + divOk).val().trim() + "<br>";
+                mensaje+= "Modelo de auto: " + $('#cmbModelo' + divOk).val().trim() + "<br>";
+                mensaje+= "Dispone de usado: " + $("#txtMonto" + divOk).val().trim() + "<br>";
             }
-            mensaje+= "Valor de cuota que puede pagar: " + $("#txtCuota").val().trim() + "<br>";
+            mensaje+= "Valor de cuota que puede pagar: " + $("#txtCuota" + divOk).val().trim() + "<br>";
 
-            mensaje+= "Consulta: " + $("#txtConsulta").val().trim() + "<br>";
+            mensaje+= "Consulta: " + $("#txtConsulta" + divOk).val().trim() + "<br>";
 
             var frmData = new FormData();
-            frmData.append("Nombre", $('#resultadoCuit').html().trim());
-            frmData.append("Email", $("#txtEmail").val().trim());
+            frmData.append("Nombre", $('#resultadoCuit' + divOk).html().trim());
+            frmData.append("Email", $("#txtEmail" + divOk).val().trim());
             frmData.append("Mensaje", mensaje);
 
             if (window.XMLHttpRequest)
